795ef63ba00dfb965937d7b3161097edfbbe12ab
Donnio Megane <donnio.megane@gmail.com>
2020-11-20 13:13:26 +0100

    2e commit

--- ci/application/views/propos.php
+++ ci/application/views/propos.php
@@ -13,18 +13,18 @@
             dans tous vos projets de vente ou d'acquisition de logements en tout genres.</p>
 
             <h2>En quoi nous pouvons vous aider ?</h2>
-            Notre expérience nous permet de vous proposer une offre adaptée au marché de l'immobilier amiénois</br>
+            <p>Notre expérience nous permet de vous proposer une offre adaptée au marché de l'immobilier amiénois</br>
             en prenant en compte vos critères pour vous proposer le meilleur. Nous vous proposons un suivi</br>
             personnalisé de votre dossier, ainsi qu'une aide juridique en cas de besoin. Nous réalisons les</br>
             estimations de vos biens et nous chargeons de leur promotion par le biais de publicité (sites et</br>
-            presses spécialisés, vitrine, etc). 
+            presses spécialisés, vitrine, etc).</p>
 
            <h3>Pour qui ?</h3>
-            Nous recevons toutes personnes se lançant dans un projet immobilier, que ce soit le premier ou non.
+            <p>Nous recevons toutes personnes se lançant dans un projet immobilier, que ce soit le premier ou non.</p>
 
             <h4>Contactez-nous !</h4>
-            Si vous souhaitez plus d'informations, n'hésitez pas à nous rendre visite dans notre agence rue</br> 
-            de Poulainville ou nous contacter via le formulaire dédié. 
+            <p>Si vous souhaitez plus d'informations, n'hésitez pas à nous rendre visite dans notre agence rue</br> 
+            de Poulainville ou nous contacter via le formulaire dédié.</p>
 
 
 
